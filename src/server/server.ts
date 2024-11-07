5337b392d57fbe7b836a6314725f5c29d3c9f976
simon <cabansagsimon@gmail.com>
2024-11-07 16:07:46 +0000

    fix: modify initialise dotenv config
    this will stop using the dev port in production environment

--- src/server/server.ts
+++ src/server/server.ts
@@ -11,7 +11,6 @@ const isProduction = process.env.NODE_ENV === "production"
 if (!isProduction) {
     dotenv.config()
 }
-dotenv.config()
 
 async function startServer() {
     const PORT = process.env.PORT
