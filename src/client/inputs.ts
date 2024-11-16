f13b62a0d639d5220b7b4c306cbfb47dda4796b4
simon <cabansagsimon@gmail.com>
2024-11-16 02:56:01 +0000

    feat: add mousemove event listenerto update mouse deltas across window

--- src/client/inputs.ts
+++ src/client/inputs.ts
@@ -1,4 +1,13 @@
-export default class Inputs {
+export class Inputs {
+    current = {
+        leftButton: false,
+        rightButton: false,
+        mouseX: 0,
+        mouseY: 0,
+        mouseXDelta: 0,
+        mouseYDelta: 0,
+    }
+    previousKeys = {}
     keys = {
         forward: false,
         backward: false,
@@ -8,14 +17,51 @@ export default class Inputs {
         space: false,
         lcontrol: false,
     }
+    previous!: {
+        leftButton: boolean
+        rightButton: boolean
+        mouseX: number
+        mouseY: number
+        mouseXDelta: number
+        mouseYDelta: number
+    } | null
 
-    constructor() {
-        document.addEventListener("keydown", (event) => {
-            this.onKeyDown(event), false
-        })
-        document.addEventListener("keyup", (event) => {
-            this.onKeyUp(event), false
-        })
+    onMouseDown(e: MouseEvent) {
+        switch (e.button) {
+            case 0: {
+                this.current.leftButton = true
+                break
+            }
+            case 2: {
+                this.current.rightButton = true
+                break
+            }
+        }
+    }
+
+    onMouseUp(e: MouseEvent) {
+        switch (e.button) {
+            case 0: {
+                this.current.leftButton = false
+                break
+            }
+            case 2: {
+                this.current.rightButton = false
+                break
+            }
+        }
+    }
+
+    onMouseMove(e: MouseEvent) {
+        this.current.mouseX = e.pageX - window.innerWidth / 2
+        this.current.mouseY = e.pageY - window.innerHeight / 2
+
+        if (this.previous == null) {
+            this.previous = { ...this.current }
+        }
+
+        this.current.mouseXDelta = this.current.mouseX - this.previous.mouseX
+        this.current.mouseYDelta = this.current.mouseY - this.previous.mouseY
     }
 
     onKeyDown(e: KeyboardEvent) {
@@ -62,5 +108,9 @@ export default class Inputs {
                 this.keys.lcontrol = true
                 break
         }
+    }
+
+    update() {
+        this.previous = { ...this.current }
     }
 }
