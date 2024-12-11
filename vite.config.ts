588910d0b417d5df8eb8a2a9c908882776de5169
simon <cabansagsimon@gmail.com>
2024-12-11 19:30:44 +0000

    fix: support top-level await for build

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from "vite"
 import path from "node:path"
 
 export default defineConfig({
+    esbuild: {
+        supported: {
+            "top-level-await": true,
+        },
+    },
     root: path.resolve(__dirname),
     server: {
         middlewareMode: true,
