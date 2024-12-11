569cb6c1a25552f851f44c7e047fe5725d60888b
simon <cabansagsimon@gmail.com>
2024-12-11 19:19:53 +0000

    feat: add CannonJS player physics and collisions

--- src/client/index.ts
+++ src/client/index.ts
@@ -12,6 +12,15 @@ import {
     LinearToneMapping,
 } from "three/src/constants.js"
 import { FirstPersonCamera } from "./first-person-camera.ts"
+import { Mesh } from "three/src/objects/Mesh.js"
+import { MeshStandardMaterial } from "three/src/materials/MeshStandardMaterial.js"
+import { BoxGeometry } from "three/src/geometries/BoxGeometry.js"
+import { SphereGeometry } from "three/src/geometries/SphereGeometry.js"
+import { Object3D, Object3DEventMap } from "three/src/core/Object3D.js"
+// import { OrbitControls } from "three/examples/jsm/Addons.js"
+import * as CANNON from "cannon-es"
+import { PointerLockControlsCannon } from "./PointerLockControlsCannon.ts"
+import { PlaneGeometry } from "three/src/geometries/PlaneGeometry.js"
 
 const galleryModelUrl = "/gallery-model"
 const environmentMapping = "/environment-mapping"
@@ -23,6 +32,15 @@ class Portfolio {
     previousRAF!: number
     serverAssets!: { [key: string]: any }
     firstPersonCamera!: FirstPersonCamera
+    world!: CANNON.World
+    controls!: PointerLockControlsCannon
+    material: any
+    gravity = -9.81
+    physicsMaterial: any
+    balls: CANNON.Body[] = []
+    ballMeshes: Mesh<SphereGeometry, any, Object3DEventMap>[] = []
+    boxes: CANNON.Body[] = []
+    boxMeshes: Mesh<BoxGeometry, any, Object3DEventMap>[] = []
 
     constructor(serverAssets: { [key: string]: any }) {
         this.serverAssets = serverAssets
@@ -44,25 +62,185 @@ class Portfolio {
 
         const fov = 60
         const aspect = window.innerWidth / window.innerHeight
-        const near = 0.8
+        const near = 0.1
         const far = 10000
         this.camera = new PerspectiveCamera(fov, aspect, near, far)
+        // this.camera.position.set(50, 10, 50)
         this.threejs.setClearColor(0x505050)
         this.threejs.toneMapping = LinearToneMapping
         const ambient = new AmbientLight(0xffffff, 0.1)
         const light = new DirectionalLight(0xffffff, 5)
         light.position.set(0, 0, 0)
+        this.scene = new Scene()
         const env = this.serverAssets["env"]
         env.mapping = EquirectangularReflectionMapping
-        this.scene = new Scene()
         this.scene.background = env
         this.scene.environment = env
         this.scene.add(ambient)
         this.scene.add(light)
-        const gallery = this.serverAssets["gallery"]
-        gallery.position.set(0, -1, 0)
+
+        const gallery: Object3D<Object3DEventMap> = this.serverAssets["gallery"]
+        // const orbit = new OrbitControls(this.camera, this.threejs.domElement)
         this.scene.add(gallery)
-        this.firstPersonCamera = new FirstPersonCamera(this.camera)
+
+        this.LoadCollisions()
+    }
+
+    LoadCollisions() {
+        this.world = new CANNON.World()
+        this.world.defaultContactMaterial.contactEquationStiffness = 1e9
+        this.world.defaultContactMaterial.contactEquationRelaxation = 4
+        const solver = new CANNON.GSSolver()
+        solver.iterations = 7
+        solver.tolerance = 0.1
+        this.world.solver = new CANNON.SplitSolver(solver)
+        this.world.gravity.set(0, this.gravity, 0)
+        this.physicsMaterial = new CANNON.Material("physics")
+        const physics_physics = new CANNON.ContactMaterial(
+            this.physicsMaterial,
+            this.physicsMaterial,
+            {
+                friction: 0.0,
+                restitution: 0.0,
+            }
+        )
+        this.world.addContactMaterial(physics_physics)
+
+        const floorGeometry = new PlaneGeometry(10, 20)
+        floorGeometry.rotateX(-Math.PI / 2)
+        const floor = new Mesh(floorGeometry, this.material)
+        floor.position.set(0, 0, 0)
+        floor.receiveShadow = true
+
+        this.material = new MeshStandardMaterial({ color: "red" })
+        const northWall = new Mesh(new PlaneGeometry(10, 8), this.material)
+        northWall.receiveShadow = true
+        northWall.position.set(0, 2, -8.11)
+
+        const westWall = new Mesh(new PlaneGeometry(17, 8), this.material)
+        westWall.geometry.rotateY(Math.PI / 2)
+        westWall.receiveShadow = true
+        westWall.position.set(-3.88, 2, 0)
+
+        const eastWall = new Mesh(new PlaneGeometry(17, 8), this.material)
+        eastWall.geometry.rotateY(-Math.PI / 2)
+        eastWall.receiveShadow = true
+        eastWall.position.set(3.68, 2, 0)
+
+        const southWall = new Mesh(new PlaneGeometry(10, 8), this.material)
+        southWall.geometry.rotateX(Math.PI)
+        southWall.receiveShadow = true
+        southWall.position.set(0, 2, 7.37)
+
+        // this.scene.add(floor, southWall, northWall, westWall, eastWall)
+
+        const { width: floorWidth, height: floorHeight } =
+            floor.geometry.parameters
+        const { x: floorX, y: floorY, z: floorZ } = floor.position
+        const { width: northWallWidth, height: northWallHeight } =
+            northWall.geometry.parameters
+        const {
+            x: northWallX,
+            y: northWallY,
+            z: northWallZ,
+        } = northWall.position
+        const { width: westWallWidth, height: westWallHeight } =
+            westWall.geometry.parameters
+        const { x: westWallX, y: westWallY, z: westWallZ } = westWall.position
+        const { width: eastWallWidth, height: eastWallHeight } =
+            eastWall.geometry.parameters
+        const { x: eastWallX, y: eastWallY, z: eastWallZ } = eastWall.position
+        const { width: southWallWidth, height: southWallHeight } =
+            southWall.geometry.parameters
+        const {
+            x: southWallX,
+            y: southWallY,
+            z: southWallZ,
+        } = southWall.position
+
+        const floorBody = new CANNON.Body({
+            mass: 0,
+            material: this.physicsMaterial,
+            position: new CANNON.Vec3(floorX, floorY, floorZ),
+        })
+
+        const northWallBody = new CANNON.Body({
+            mass: 0,
+            material: this.physicsMaterial,
+            position: new CANNON.Vec3(northWallX, northWallY, northWallZ),
+        })
+        const westWallBody = new CANNON.Body({
+            mass: 0,
+            material: this.physicsMaterial,
+            position: new CANNON.Vec3(westWallX, westWallY, westWallZ),
+        })
+        const eastWallBody = new CANNON.Body({
+            mass: 0,
+            material: this.physicsMaterial,
+            position: new CANNON.Vec3(eastWallX, eastWallY, eastWallZ),
+        })
+        const southWallBody = new CANNON.Body({
+            mass: 0,
+            material: this.physicsMaterial,
+            position: new CANNON.Vec3(southWallX, southWallY, southWallZ),
+        })
+
+        floorBody.addShape(
+            new CANNON.Box(
+                new CANNON.Vec3(floorWidth / 2, floorHeight / 2, 0.1)
+            )
+        )
+        northWallBody.addShape(
+            new CANNON.Box(
+                new CANNON.Vec3(northWallWidth / 2, northWallHeight / 2, 0.1)
+            )
+        )
+        westWallBody.addShape(
+            new CANNON.Box(
+                new CANNON.Vec3(westWallWidth / 2, westWallHeight / 2, 0.1)
+            )
+        )
+        eastWallBody.addShape(
+            new CANNON.Box(
+                new CANNON.Vec3(eastWallWidth / 2, eastWallHeight / 2, 0.1)
+            )
+        )
+        southWallBody.addShape(
+            new CANNON.Box(
+                new CANNON.Vec3(southWallWidth / 2, southWallHeight / 2, 0.1)
+            )
+        )
+
+        floorBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0)
+        northWallBody.quaternion.setFromEuler(0, 0, 0)
+        westWallBody.quaternion.setFromEuler(0, Math.PI / 2, 0)
+        eastWallBody.quaternion.setFromEuler(0, -Math.PI / 2, 0)
+        southWallBody.quaternion.setFromEuler(Math.PI, 0, 0)
+
+        this.world.addBody(floorBody)
+        this.world.addBody(northWallBody)
+        this.world.addBody(westWallBody)
+        this.world.addBody(eastWallBody)
+        this.world.addBody(southWallBody)
+
+        const playerBody = new CANNON.Body({
+            mass: 1,
+            material: this.physicsMaterial,
+        })
+        playerBody.addShape(new CANNON.Box(new CANNON.Vec3(0.5, 1.2, 0.5)))
+        playerBody.position.set(0, 5, 0)
+        playerBody.linearDamping = 0.9
+        this.world.addBody(playerBody)
+
+        this.controls = new PointerLockControlsCannon(
+            this.camera,
+            playerBody,
+            this.gravity
+        )
+
+        this.scene.add(this.controls.getObject())
+        this.controls.enabled = true
+
         this.threejs?.render(this.scene, this.camera)
     }
 
@@ -81,11 +259,21 @@ class Portfolio {
 
     Step(timeElapsed: number, lastUpdate: number) {
         const _timeElapsed = Math.min(1 / 30, timeElapsed * 0.001)
+
         if (Math.abs(_timeElapsed - (lastUpdate ?? 0)) >= 0.0021) {
-            this.firstPersonCamera.deltaTime = _timeElapsed
-            this.firstPersonCamera.updateHeadBob()
-            this.firstPersonCamera.updateTranslation()
-            this.firstPersonCamera.updateCamera()
+            if (this.controls.enabled) {
+                this.world.step(_timeElapsed)
+            }
+            this.world.step(_timeElapsed, lastUpdate)
+            this.controls.update(_timeElapsed)
+            // console.log(
+            //     this.controls.moveForward,
+            //     this.controls.moveBackward,
+            //     this.controls.moveLeft,
+            //     this.controls.moveRight
+            // )
+            // this.firstPersonCamera.deltaTime = _timeElapsed
+            // this.firstPersonCamera.update()
             lastUpdate = _timeElapsed
         }
     }
